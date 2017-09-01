5413f5e161c4df373ed5904dc810ec54185eba60
Souborno <msouborno>
2017-09-01 20:30:19 +0530

    Initial Commit

--- src/java_download_manager/Java_Download_Manager.java
+++ src/java_download_manager/Java_Download_Manager.java
@@ -15,7 +15,7 @@ public class Java_Download_Manager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
